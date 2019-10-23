84201c9b38fde268a487cbdd0bd35db333f996f5
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2019-10-23 17:47:25 -0300

    Fila errada

---
+++ Versões/Parte3/fila.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "fila.h"
+
+
+fila* create_f(unsigned long int tam0){
+	int tam1 = sqrt(tam0);
+	int tamanho = floor(tam1);
+
+	fila* f = (fila*) malloc(sizeof(fila));
+ 	if(f!=NULL){
+ 		f->inicio = 0;
+ 		f->fim = tamanho-1;
+ 		f->qtd = 0;
+ 		f->tam = tamanho;
+ 		f->vetor = (no**) malloc(sizeof(no*)*f->tam);
+ 		if(f->vetor == NULL) return NULL;
+ 		else return f;
+ 	}
+ 	else return NULL;
+}
+
+//Função que retorna 1 se a fila está cheia ou 0 senão
+int full_f(fila *f){
+	if(f->qtd == f->tam) return 1;
+	else return 0;
+}
+
+//Função para checar se a fila está vazia
+int isEmpty_f(fila *f){
+	if(f->qtd == 0) return 1;
+	else return 0;
+}
+
+//Função para enfileirar elemento
+void push_f(fila *f,no* x){
+	if(full_f(f)==1) printf("FILA CHEIA!");
+	else{
+		  f->fim = (f->fim+1)%f->tam;
+		  f->vetor[f->fim] = x;
+		  f->qtd++;
+	}
+}
+
+//Função para desenfileirar primeiro elemento da fila (NULL se vazia)
+no* pop_f(fila *f){
+ 	if(isEmpty_f(f)==1){
+    return NULL;
+  }
+ 	else{
+ 		unsigned long int x = f->inicio;
+ 		f->inicio = (f->inicio+1)%f->tam;
+ 		f->qtd--;
+ 		return f->vetor[x];
+ 	}
+}
+
+//Retorna primeiro elemento da fila (NULL - Caso fila vazia)
+no* front_f(fila*f){
+	if(isEmpty_f(f)==1) return NULL;
+	else return f->vetor[f->inicio];
+}
+
+//Retorna tamanho da fila
+unsigned long int size_f(fila* f){
+  return f->tam;
+}
+
+//Procura pela primeira ocorrência de um CPF na fila, e retorna-a (NULL Caso não encontrado ou Fila Vazia)
+no* search_f (fila* f,unsigned long int cpf){
+  no* resultado = NULL;
+  if(isEmpty_f(f)==0){
+    int flag=0;
+    for(unsigned long int i =0; i<f->tam;i++){
+      no* aux = front_f(f);
+      if(aux->cpf == cpf && flag == 0){
+          resultado = aux;
+      }
+      pop_f(f);
+      push_f(f,aux);
+    }
+  }
+  return resultado;
+}
+
+//Função para destuir a fila e todos os nós referentes
+void destroy_f(fila* f){
+  while(isEmpty_f(f)==0){
+    no* cliente  = pop_f(f);
+    free(cliente);
+  }
+  free(f->vetor);
+  free(f);
+}
--- Versões/Parte3/principal.c
+++ Versões/Parte3/principal.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include "pilha.h"
 #include "lista.h"
+#include "fila.h"
 
 int main(){
-    lista *l = create_l();
 
-    long long int cpf;                  //Criou variáveis para ler valores
-    scanf("%lld", &cpf);
+    unsigned long int cpf, cpft;
+    long int tam;
+    char c;                  //Criou variáveis para ler valores
+    scanf("%lu %lu %c %lu", &cpf, &cpft, &c, &tam);
 
+
+    fila *f = create_f(tam);
 
     // Imprime os valores no topo da pilha, como os nós foram apenas criados mas não
     // foi feito push, dará falha de segmentação, afinal, os nós não foram ligados
@@ -19,35 +23,42 @@ int main(){
     // printf("Vazia: %d \n",isEmpty_l(l));  //Verifica se a pilha está vazia
 
     //Um ponteiro para cada cliente receber o retorno da função create_n
-    nol *cliente0 = create_nol(cpf);
-    nol *cliente1 = create_nol(cpf+1);
-    nol *cliente2 = create_nol(cpf+2);
-    nol *cliente3 = create_nol(cpf+3);
-    nol *cliente4 = create_nol(cpf+5);
-    nol *cliente5 = create_nol(cpf+4);
+    no *cliente0 = create_no(cpf+1, cpft+1, c, 100);
+    no *cliente1 = create_no(cpf+2, cpft+2, c, 200);
+    no *cliente2 = create_no(cpf+3, cpft+3, c, 300);
+    no *cliente3 = create_no(cpf+4, cpft+4, c, 400);
+    no *cliente4 = create_no(cpf+5, cpft+5, c, 500);
+    no *cliente5 = create_no(cpf+6, cpft+6, c, 600);
 
     //Cria dois ponteiros auxiliares para facilitar o push e o printf
-    nol *ax0 = cliente0;
-    nol *ax1 = cliente1;
-    nol *ax2 = cliente2;
-    nol *ax3 = cliente3;
-    nol *ax4 = cliente4;
-    nol *ax5 = cliente5;
+    no *ax0 = cliente0;
+    no *ax1 = cliente1;
+    no *ax2 = cliente2;
+    no *ax3 = cliente3;
+    no *ax4 = cliente4;
+    no *ax5 = cliente5;
 
     //Empilha os nós de cada cliente imprime para testar
-    push_l(l, ax4);
-    push_l(l, ax2);
-    push_l(l, ax0);
-    push_l(l, ax5);
-    push_l(l, ax1);
-    push_l(l, ax3);
+    push_f(f, ax4);
+    push_f(f, ax2);
+    push_f(f, ax0);
+    push_f(f, ax5);
+    push_f(f, ax1);
+    push_f(f, ax3);
 
-    while(isEmpty_l(l)==0){
+    while(isEmpty_f(f)==0){
+
+
         int x = l->primeiro->cpf;
         printf("%d\n",x);
-        pop_l(l, l->primeiro);
-        if(isEmpty_l(l) == 1) break;
+        pop_f(l, l->primeiro);
+        if(isEmpty_f(l) == 1) break;
     }
+
+    // destroy_l(l);
+    // int x = l->primeiro->cpf;
+    // printf("%d\n", x);
+
     // printf("debug\n");
 
     //Verificando se a função pop_p está retornando corretamente
